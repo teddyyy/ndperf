795e6fb2da0891a89ec7e760bd3f1765d58d5705
teddy <teddy@sfc.wide.ad.jp>
2017-05-19 01:21:18 +0900

    ittan commit

--- test/netlink/create/create-macvlan.c
+++ test/netlink/create/create-macvlan.c
@@ -1,4 +1,7 @@
+#include <unistd.h>
 #include <netinet/ether.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 
 #include <netlink/netlink.h>
 #include <netlink/route/link.h>
@@ -6,13 +9,39 @@
 
 #include <linux/netlink.h>
 
+static int
+ifup(char *name)
+{
+	int fd;
+	struct ifreq ifr;
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	strncpy(ifr.ifr_name, name, IFNAMSIZ-1);
+	if (ioctl(fd, SIOCGIFFLAGS, &ifr) != 0) {
+		perror("ioctl");
+		return -1;
+	}
+
+	ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
+
+	if (ioctl(fd, SIOCSIFFLAGS, &ifr) != 0) {
+		perror("ioctl");
+		return -1;
+	}
+
+	close(fd);
+
+	return 0;
+}
+
 int
 main(int argc, char *argv[])
 {
 	struct rtnl_link *link;
 	struct nl_cache *link_cache;
 	struct nl_sock *sock;
-	int err, master_index;
+	int i, err, master_index;
 
 	sock = nl_socket_alloc();
 	if ((err = nl_connect(sock, NETLINK_ROUTE)) < 0) {
@@ -30,17 +59,23 @@ main(int argc, char *argv[])
 		return -1;
 	}
 
-	link = rtnl_link_macvlan_alloc();
-	rtnl_link_set_link(link, master_index);
+	for (i = 0; i < 3; i++) {
+		link = rtnl_link_macvlan_alloc();
+		rtnl_link_set_link(link, master_index);
 
-	rtnl_link_macvlan_set_mode(link, rtnl_link_macvlan_str2mode("private"));
+		rtnl_link_macvlan_set_mode(link,
+				   rtnl_link_macvlan_str2mode("private"));
 
-	if ((err = rtnl_link_add(sock, link, NLM_F_CREATE)) < 0) {
-		nl_perror(err, "Unable to add link");
-		return err;
+		rtnl_link_set_name(link, "vif%d");
+		if ((err = rtnl_link_add(sock, link, NLM_F_CREATE)) < 0) {
+			nl_perror(err, "Unable to add link");
+			return err;
+		}
+
+		ifup(rtnl_link_get_name(link));
+
+		rtnl_link_put(link);
 	}
-
-	rtnl_link_put(link);
 
 	nl_close(sock);
 
