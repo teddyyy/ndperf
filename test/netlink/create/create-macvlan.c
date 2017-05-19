650fcd230bdeb03f2c0377087f826483221b22ae
teddy <teddy@sfc.wide.ad.jp>
2017-05-20 01:25:39 +0900

    add set_ip6addr function

--- test/netlink/create/create-macvlan.c
+++ test/netlink/create/create-macvlan.c
@@ -2,6 +2,7 @@
 #include <netinet/ether.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <arpa/inet.h>
 
 #include <netlink/netlink.h>
 #include <netlink/route/link.h>
@@ -9,13 +10,20 @@
 
 #include <linux/netlink.h>
 
+struct in6_ifreq
+{
+	struct in6_addr ifr6_addr;
+	u_int32_t ifr6_prefixlen;
+	int ifr6_ifindex;
+};
+
 static
 char* itoa(int num)
 {
 	int count = 1;
 	int n = num;
 
-	while(n > 10) {n = n / 10; count++;}
+	while (n > 10) {n = n / 10; count++;}
 
 	char *str = malloc(count + 1);
 
@@ -23,12 +31,38 @@ char* itoa(int num)
 
 	n = num;
 
-	for(int i = count-1; i >= 0; i--) {
+	for (int i = count-1; i >= 0; i--) {
 		str[i] =  (n % 10) + '0';
 		n = n / 10;
 	}
 
 	return str;
+}
+
+static
+int set_ip6addr(char *name, char *ip6addr, int prefixlen)
+{
+	int fd;
+	struct in6_ifreq ifr;
+	int ret;
+
+	fd = socket(AF_INET6, SOCK_DGRAM, 0);
+
+	memset(&ifr, 0, sizeof(ifr));
+	inet_pton(AF_INET6, ip6addr, &ifr.ifr6_addr);
+
+	ifr.ifr6_prefixlen = prefixlen;
+	ifr.ifr6_ifindex = if_nametoindex(name);
+
+	ret = ioctl(fd, SIOCSIFADDR, &ifr);
+	if (ret < 0) {
+		perror("ioctl");
+		return -1;
+	}
+
+	close(fd);
+
+	return 0;
 }
 
 
@@ -64,8 +98,9 @@ main(int argc, char *argv[])
 	struct rtnl_link *link;
 	struct nl_cache *link_cache;
 	struct nl_sock *sock;
-	int i, err, master_index;
+	int i, n, err, master_index;
 	char ifname[16] = "";
+	char addr[48] = "";
 
 	sock = nl_socket_alloc();
 	if ((err = nl_connect(sock, NETLINK_ROUTE)) < 0) {
@@ -78,7 +113,7 @@ main(int argc, char *argv[])
 		return err;
 	}
 
-	if (!(master_index = rtnl_link_name2i(link_cache, "enp0s3"))) {
+	if (!(master_index = rtnl_link_name2i(link_cache, "enp0s8"))) {
 		fprintf(stderr, "Unable to lookup enp0s3");
 		return -1;
 	}
@@ -97,6 +132,12 @@ main(int argc, char *argv[])
 			nl_perror(err, "Unable to add link");
 			return err;
 		}
+
+		/* Since it is used with the getway and the DUT,
+		   0 and 1 are not used */
+		n = i + 2;
+		sprintf(addr, "2001:2:0:1::%s", itoa(n));
+		set_ip6addr(ifname, addr, 64);
 
 		ifup(rtnl_link_get_name(link));
 
